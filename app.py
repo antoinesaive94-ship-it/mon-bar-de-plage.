579c388e94e0bce45f5bc27384ff9f626ca4c48c
antoinesaive94-ship-it <antoinesaive94@gmail.com>
2026-01-31 23:57:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -55,5 +55,7 @@ def reservation():
     return render_template('reservation.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # On force le port pour Render
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
